6f4d48079eb939d7694fb3b7b02e49dc101f0bd5
Aleksandr Pankrashin <alexp995@yandex.ru>
2025-08-01 18:02:34 +0300

    final work parser

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int sumN = 0;
+        while (true) {
+            System.out.println("Введите путь: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists != true) {
+                System.out.println("Файл не найден");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Некорректный путь");
+                continue;
+            }
+
+            sumN++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + sumN);
+
+        }
     }
 }
+
+
+
+
+
+
